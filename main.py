b24a318ed60a8b655a43c865d9e5ba469bd4d1e4
Astrid Yu <astrid@astrid.tech>
2021-02-03 16:09:38 -0800

    Printer status scraping

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
 #!/usr/bin/env python3
-
 import os
 
 import requests
 import structlog
+from requests import RequestException
 
 logger = structlog.get_logger(__name__)
 
-SNAPSHOT_URL = os.environ["SNAPSHOT_URL"]
-PRINTER_ENDPOINT = os.environ["PRINTER_ENDPOINT"]
-USER = os.environ["USER"]
-PASSWORD = os.environ["PASSWORD"]
+SNAPSHOT_URL = os.getenv("SNAPSHOT_URL")
+OCTOPRINT_KEY = os.getenv("OCTOPRINT_KEY")
+OCTOPRINT_ROOT = os.getenv("OCTOPRINT_ROOT")
+PRINTER_ENDPOINT = os.getenv("PRINTER_ENDPOINT")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+
+
+def get_printer_status():
+    headers = {'X-Api-Key': OCTOPRINT_KEY}
+    printer_info_url = OCTOPRINT_ROOT + 'api/printer'
+    logger.info("Fetching printer data", url=printer_info_url)
+    printer = requests.get(printer_info_url, headers=headers)
+    logger.debug("Endpoint returned printer data", status=printer.status_code)
+    if printer.status_code == 409:
+        return "Not currently printing"
+
+    printer_json = printer.json()
+    logger.debug("Printer state is JSON", response=printer_json)
+    flags = printer_json['state']['flags']
+    printing = flags['printing']
+    if not printing:
+        return "Not currently printing"
+
+    job_info_url = OCTOPRINT_ROOT + 'api/job'
+    logger.info("Fetching job data")
+    job = requests.get(job_info_url, headers=headers)
+    logger.debug("Endpoint returned job data", status=job.status_code, response=job.json)
+
+    progress = job.json()['progress']
+    completion = progress['completion'] * 100
+    print_time = progress['printTime']
+    return f'~{completion:02d}% of the way through a job started at {print_time}'
 
 
 def main():
-    logger.info("Fetching snapshot", url=SNAPSHOT_URL)
-    snapshot = requests.get(SNAPSHOT_URL).content
+    try:
+        printer_status = get_printer_status()
+    except Exception as e:
+        logger.error("Error fetching snapshot", error=e)
+        printer_status = 'Failed to retrieve printer status'
 
-    logger.info("PATCHing server image", url=PRINTER_ENDPOINT, username=USER, bytes=len(snapshot))
+    try:
+        logger.info("Fetching snapshot", url=SNAPSHOT_URL)
+        snapshot = requests.get(SNAPSHOT_URL).content
+    except RequestException as e:
+        logger.error("Error fetching snapshot", error=e)
+        snapshot = None
+
+    logger.info("PATCHing server image", url=PRINTER_ENDPOINT, username=USER, image_bytes=len(snapshot), printer_status=printer_status)
     response = requests.patch(PRINTER_ENDPOINT, auth=(USER, PASSWORD),
-                              files={'image': ('snapshot.jpg', snapshot, 'image/jpg')})
+                              files={'status': printer_status,
+                                     'image': None if snapshot is None else ('snapshot.jpg', snapshot, 'image/jpg')})
 
     if response.status_code == 200:
         logger.info("Successfully uploaded snapshot", content=response.json())
@@ -28,4 +68,15 @@ def main():
 
 
 if __name__ == '__main__':
+    structlog.configure(
+        processors=[
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.processors.JSONRenderer()
+        ]
+    )
     main()
